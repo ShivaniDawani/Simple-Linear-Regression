ea208e4b7bd3617cdca25699c216021260fb1655
ShivaniDawani <shivanidawani.bscskdkf22@iba-suk.edu.pk>
2025-04-15 00:27:11 -0700

    add button label

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ with open("model.pkl", "rb") as file:
     model=pickle.load(file)
 
     user_input=st.number_input("experience years", min_value=0.0, max_value= 50.0, value=0.0, step=0.1)
-    st.success(model.predict([[user_input]])[0])
+    if st.button("Predict Salary"):
+        st.success(model.predict([[user_input]])[0])
