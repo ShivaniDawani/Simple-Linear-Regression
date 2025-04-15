db8d322e2c7c9dea80f6550658c354e22bbebfa4
ShivaniDawani <shivanidawani.bscskdkf22@iba-suk.edu.pk>
2025-04-14 23:59:31 -0700

    app completed

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pickle
+st.title('Salary Pred')
+
+
+with open("model.pkl", "rb") as file:
+    model=pickle.load(file)
+
+    user_input=st.number_input("experience years", min_value=0.0, max_value= 50.0, value=0.0, step=0.1)
+    st.success(model.predict([[user_input]])[0])
